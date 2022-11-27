fd44ed9e66acc18db7e8061d8521cd80e3ac913b
vincejs77 <vinjobs77@gmail.com>
2022-11-27 18:18:17 +0200

    update :  all

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,16 @@ for (let i = 0; i < competences.length; i++) {
 	div.classList.add("box");
 	div.innerHTML = `
     <div> <img src="${competences[i].icon}" /> </div>
+	<h3> ${competences[i].title} </h3>
+	<p> ${competences[i].desc} </p>
     `;
 	competencesDiv.appendChild(div);
 }
+
+const menuBtn = document.querySelector("#menu-mobile");
+
+const menu = document.querySelector("#menu");
+
+menuBtn.addEventListener("click", function (event) {
+	menu.classList.toggle("is-visible");
+});
