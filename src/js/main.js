d079c365b3491c88fd744bfe0130b5660ab8c1d6
vincejs77 <vinjobs77@gmail.com>
2022-11-28 05:33:10 +0200

    update :  all 002

--- src/js/main.js
+++ src/js/main.js
@@ -33,3 +33,45 @@ menuBtn.addEventListener("click", function (event) {
 	menu.classList.toggle("is-visible");
 	overlay.classList.toggle("is-visible");
 });
+
+/*
+Affichier la barre de progression au stroll
+
+*/
+
+const scrollProgress = document.getElementById("scroll-progress");
+const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+	const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+/*
+Animation d'éléments apparaissant au scroll
+*/
+
+const observer = new IntersectionObserver((entries) => {
+	for (const entry of entries) {
+		// console.log(entry.target, entry.isVisible, entry.isIntersecting);
+		if (entry.isIntersecting) {
+			entry.target.classList.add("is-animated");
+			entry.target.animate(
+				[
+					{ transform: "scale(0.97)", opacity: 0.5 },
+					{ transform: "scale(1)", opacity: 1 }
+				],
+				{
+					duration: 500,
+					iterations: 1,
+					fill: "both"
+				}
+			);
+			// observer.unobserve(entry.target); // pour observer une seule fois
+		}
+	}
+});
+
+document.querySelectorAll(".animated-on-scroll").forEach((el) => {
+	observer.observe(el);
+});
